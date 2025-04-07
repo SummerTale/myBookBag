bad94a7e6be7dc317dc176e7cf2553d8124b6e71
Kavya Verma <kavyaverma5601@gmail.com>
2025-04-07 11:08:21 -0400

    Use bcrypt with 12 salt rounds for stronger password hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,7 @@ exports.registerUser = async (req, res) => {
         }
 
         // Hash the password
-        const salt = await bcrypt.genSalt(10);
+        const salt = await bcrypt.genSalt(12);
         const hashedPassword = await bcrypt.hash(password, salt);
 
         // Create a new user
